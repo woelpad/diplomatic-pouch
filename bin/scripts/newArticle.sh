f86677a69469589ef50209181050bba148a4666a
Brian Lovelace <brianlovelace128@gmail.com>
2018-06-02 17:14:12 -0700

    create work script

---
+++ bin/scripts/newArticle.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ARTICLE=$1
+AUTHOR=$2
+
+if [ -z "$ARTICLE" ]; then
+  echo "Error: Missing arg 1 (ARTICLE NAME)"
+  exit
+fi
+
+if [ -z "$AUTHOR" ]; then
+  echo "Error: Missing arg 1 (AUTHOR NAME)"
+  exit 1
+fi
+
+if [ ! -d "../Work/Template/" ]; then
+	echo "Error: Make sure you are in a seasonal directory (F2018M)"
+	exit 1
+fi
+
+if [ ! -d "./images" ]; then
+  mkdir images
+fi
+
+mkdir $AUTHOR;
+cp ../Work/Template/template.html $AUTHOR/$ARTICLE
+mkdir images/$ARTICLE
